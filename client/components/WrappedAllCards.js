274fe5078953b0f1993d76c4ad5914ec4bbda7ed
Seoung_Yeon Lee <alexlee131005@gmail.com>
2019-03-10 05:37:21 -0500

    remove unused component function

--- client/components/WrappedAllCards.js
+++ client/components/WrappedAllCards.js
@@ -3,18 +3,7 @@ import querystring from 'query-string'
 import {sortByCost} from '../store/cardOrder'
 import AllCards from './allCards'
 export default class WrappedAllCards extends Component {
-  componentDidMount() {
-    console.log('mounting')
-    //this.props.sortByCost(Object.values(this.props.allCards))
-  }
-
   render() {
-    // parse the query params to see if there was any filter or sort
-    //const {filter, sort} = querystring.parse(this.props.location.search)
-    // on default: no sort or filter
-    //if (!filter && !sort) {
-    //this.props.sortByCost(Object.values(this.props.allCards))
-    //}
     const cardOrder = this.props.allCards
       ? sortByCost(Object.values(this.props.allCards))
       : []
