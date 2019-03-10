6ea5859781fff680f946d81a384a02d0715d034d
Seoung_Yeon Lee <alexlee131005@gmail.com>
2019-03-10 06:39:22 -0500

    clean-up working code

--- client/components/allCards.js
+++ client/components/allCards.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {Container, Row} from 'react-bootstrap'
-import {InlineCard} from './inlineCard'
+import InlineCard from './inlineCard'
 
 export default class AllCards extends Component {
   render() {
