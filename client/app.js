df0cc678ab4442de061173568d52680a9047dee0
Alex <alexrgura@gmail.com>
2019-03-11 13:54:11 -0500

    implemented working analysis" "

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {DragDropContext} from 'react-dnd'
 import TouchBackend from 'react-dnd-touch-backend'
-import HTML5Backend from 'react-dnd-html5-backend'
+
 import {Navbar} from './components'
 import Routes from './routes'
 
